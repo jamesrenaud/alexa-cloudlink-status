8f181fdc2f7d6a69dee040abece7199e73771214
James Renaud <james.renaud@mitel.com>
2018-05-23 15:33:00 -0400

    Latest fun

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const request = require('request');
 let skill;
 let incidentTitle;
 let incidentMessage;
+let incidentStatus;
+
+let statusResponses = [
+    "The incident is currently scheduled.",
+    "We're currently investigating the issue.",
+    "The issue has been identified and we are working towards a resolution.",
+    "The issue has been resolved and we are monitoring for any re-occurrences."
+]
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
@@ -57,20 +65,50 @@ const StatusIntentIntentHandler = {
     handle(handlerInput) {
 
         let speechText;
+        return CheckForBadComponents()
+            .then(data => {
+                if (data) {
+                    speechText = 'The CloudLink cloud is currently sunny, blue skies, and rainbows. All components are reporting operational. <say-as interpret-as="interjection">Booya!</say-as>';
+                }
+                else {
+                    let tempdate = incidentStatus.split(' ');
+                    let temptime = tempdate[1].split(':');
+                    let date = tempdate[0].replace(/-/g, '').toString();
+                    let time = formatAMPM(new Date(tempdate));
+                    speechText = '<say-as interpret-as="interjection">uh oh!</say-as> <p>A component is reporting an unhealthy state. <say-as interpret-as="interjection">shucks!</say-as></p><p> The affected component is ' + incidentTitle + '</p> <p>The component is currently reporting a status of ' + incidentMessage + '</p> <p>The component status was updated on <say-as interpret-as="date">' + date + '</say-as> at ' + time + '</p>';
+                }
+            })
+            .then(data => {
+                console.log(speechText);
+                return handlerInput.responseBuilder
+                    .speak(speechText)
+                    .getResponse();
+            })
+            .catch(err => {
+                speechText = 'Im having trouble determining the forecast of the cloud right now, please try again';
+                return handlerInput.responseBuilder
+                    .speak(speechText)
+                    .getResponse();
+            })
+    }
+};
 
-        // speechText = 'The CloudLink cloud is currently <say-as interpret-as="expletive">fucking</say-as> sunny, with blue skies, and rainbows. <say-as interpret-as="interjection">Booya!</say-as>';
-        // return handlerInput.responseBuilder
-        //     .speak(speechText)
-        //     .getResponse();
+const CheckForIncidentsHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'IncidentCheckIntent';
+    },
+    handle(handlerInput) {
+
+        let speechText;
 
         return CheckForIncidents()
             .then(data => {
                 if (data) {
-                    speechText = 'The CloudLink cloud is currently <say-as interpret-as="expletive">trend</say-as>ing sunny, blue skies, and rainbows. <say-as interpret-as="interjection">Booya!</say-as>';
-
+                    speechText = '<p>There are currently no incidents being reported on the CloudLink cloud.</p> <p><say-as interpret-as="interjection">Booya!</say-as></p>'
                 }
                 else {
-                    speechText = '<p>An incident has been reported on the CloudLink Cloud. <say-as interpret-as="interjection">dun dun dun!</say-as></p><p> The title of the incident is ' + incidentTitle + '</p> <p>The message of the incident is ' + incidentMessage + '</p><p> We are currently working on a fix</p> <audio src="https://s3.amazonaws.com/ask-soundlibrary/battle/amzn_sfx_battle_group_clanks_01.mp3"/>';
+                    speechText = '<p>An incident has been reported on the CloudLink Cloud. <say-as interpret-as="interjection">dun dun dun!</say-as></p><p> The title of the incident is ' + incidentTitle + '</p> <p>The message of the incident is ' + incidentMessage + '</p> <p>' + statusResponses[incidentStatus] + '</p>';
                 }
             })
             .then(data => {
@@ -185,6 +223,51 @@ function CheckForIncidents() {
                             isCloudHealthy = false;
                             incidentTitle = incident.name;
                             incidentMessage = incident.message;
+                            incidentStatus = incident.status;
+                        }
+                    })
+                    resolve(isCloudHealthy);
+                }
+                else {
+                    let data = JSON.parse(response.body);
+                    reject(data);
+                }
+            }
+        })
+    })
+}
+
+
+function CheckForBadComponents() {
+    return new Promise((resolve, reject) => {
+        let statusCreds = {
+            url: "https://status.dev.mitel.io",
+            apiKey: "sTSldlMMXfMMdh4VOVy5"
+        }
+
+        var options = {
+            url: statusCreds.url + '/api/v1/components?per_page=100',
+            method: 'GET',
+            headers: {
+                'X-Cachet-Token': statusCreds.apiKey
+            }
+        }
+
+        request(options, function (error, response, body) {
+            if (error) {
+                console.log(error);
+                reject(error);
+            }
+            else {
+                if (response.statusCode == 200) {
+                    let data = JSON.parse(response.body);
+                    let isCloudHealthy = true;
+                    data.data.find(function (component) {
+                        if (component.status_name != 'Operational') {
+                            isCloudHealthy = false;
+                            incidentTitle = component.name
+                            incidentMessage = component.status_name;
+                            incidentStatus = component.updated_at;
                         }
                     })
                     resolve(isCloudHealthy);
@@ -223,10 +306,22 @@ exports.handler = async function (event, context) {
                 HelpIntentHandler,
                 CancelAndStopIntentHandler,
                 SessionEndedRequestHandler,
-        )
+                CheckForIncidentsHandler
+            )
             .addErrorHandlers(ErrorHandler)
             .create();
     }
 
     return skill.invoke(event, context);
+}
+
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ' ' + minutes + ' ' + ampm;
+    return strTime;
 }
